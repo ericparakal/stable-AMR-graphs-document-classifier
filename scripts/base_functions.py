d8bb9d2c45fd01d6e2dc74829a3161cbdeb663fb
ericparakal2396 <ericparakal@gmail.com>
2024-06-02 12:57:04 +0300

    updated base functions script

--- scripts/base_functions.py
+++ scripts/base_functions.py
@@ -20,3 +20,31 @@ def multiple_subsumption_check(graph, subgraphs):
 
     multiple_subsumption = 1
     return multiple_subsumption
+
+
+def find_graph_pattern_weight(graph_pattern):
+    graph_pattern_weight = 0
+
+    for i in range(len(graph_pattern['subgraphs'])):
+        graph_pattern_weight += (len(graph_pattern['subgraphs'][i].nodes) * graph_pattern['supports'][i])
+
+    return graph_pattern_weight
+
+
+def find_size(subgraphs):
+    size = 0
+
+    for subgraph in subgraphs:
+        size += len(subgraph.nodes)
+
+    return size
+
+
+def find_maximal_degree(subgraphs):
+    maximal_degree = 0
+
+    for subgraph in subgraphs:
+        if sorted(subgraph.degree, key=lambda x: x[1], reverse=True)[0][1] > maximal_degree:
+            maximal_degree = sorted(subgraph.degree, key=lambda x: x[1], reverse=True)[0][1]
+
+    return maximal_degree
